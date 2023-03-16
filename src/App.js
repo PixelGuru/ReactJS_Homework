93af72c896984b742624cc30706375e21053eaec
ASUS\Administrator <vduy238@gmail.com>
2023-03-16 14:38:50 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Lession2 from "./components/Lession2";
+import "antd/dist/reset.css";
+import "./App.css";
 
 function App() {
   return <Lession2 />;
--- src/components/Lession2/index.js
+++ src/components/Lession2/index.js
@@ -1,22 +1,87 @@
-// const DEFAULT_USER = { email: "", password: "" };
-const Lession2 = () => {
-  // const [formData, setFormData] = useState(DEFAULT_USER);
+// import { Form, Input, Button } from "antd";
+
+// ///Form Login
+// const Lession2 = (props) => {
+//   const [form] = Form.useForm();
+
+//   const onSubmit = async () => {
+//     const values = await form.validateFields();
+//     console.log(values);
+//     // do somethings
+//   };
+
+//   return (
+//     <Form form={form} layout="vertical">
+//       <Form.Item
+//         label="Email"
+//         name="email"
+//         rules={[
+//           { required: true, message: "Please enter your Email!" },
+//           { type: "email", message: "Please enter your password!" },
+//         ]}
+//       >
+//         <Input style={{ width: "20%" }} />
+//       </Form.Item>
+
+//       <Form.Item
+//         label="Password"
+//         name="password"
+//         rules={[{ required: true, message: "Please enter your password!" }]}
+//       >
+//         <Input.Password style={{ width: "20%" }} />
+//       </Form.Item>
+
+//       <Form.Item>
+//         <Button type="primary" onClick={onSubmit}>
+//           Submit
+//         </Button>
+//       </Form.Item>
+//     </Form>
+//   );
+// };
+
+// export default Lession2;
+
+import { Form, Input, Button, message } from "antd";
+
+const Lession2 = (props) => {
+  const [form] = Form.useForm();
+
+  const onSubmit = async () => {
+    const values = await form.validateFields();
+    console.log(values);
+    // do somethings
+    message.success("Login successful!");
+  };
+
   return (
-    <div>
-      <input
+    <Form form={form} layout="vertical">
+      <Form.Item
+        label="Email"
         name="email"
-        type="text"
-        // value={formData.email}
-        // onChange={onChange}
-      ></input>
-      <input
+        rules={[
+          { required: true, message: "Please enter your Email!" },
+          { type: "email", message: "Please enter a valid email address!" },
+        ]}
+      >
+        <Input style={{ width: "20%" }} />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
         name="password"
-        type="password"
-        // value={formData.password}
-        // onChange={onChange}
-      ></input>
-      <button>Login</button>
-    </div>
+        rules={[{ required: true, message: "Please enter your password!" }]}
+      >
+        <Input.Password style={{ width: "20%" }} />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" onClick={onSubmit}>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
+
 export default Lession2;
