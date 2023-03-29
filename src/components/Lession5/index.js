61f7adb34541b5ac93b0c6159c8118afe6952ef6
PixelGuru <95393930+PixelGuru@users.noreply.github.com>
2023-03-29 11:30:10 +0700

    Update index.js

--- src/components/Lession5/index.js
+++ src/components/Lession5/index.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import ListProducts from "./ListProducts";
 
 const Lession5 = () => {
-  const [users] = useState({
+  const [user] = useState({
     name: "Nguyen Van A",
     email: "nguyenvana@gmail.com",
   });
@@ -15,7 +15,7 @@ const Lession5 = () => {
   return (
     <div>
       <div>
-        <div>{`Khách hàng: ${users.name} có Email: ${users.email} `}</div>
+        <div>{`Khách hàng: ${user.name} có Email: ${user.email} `}</div>
 
         <ListProducts products={products} />
       </div>
